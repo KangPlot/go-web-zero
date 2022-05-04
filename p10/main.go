41221ba1f1fd34d8fe8cd4572db31ddf309f1ffc
xpengkang <xpengkang@outlook.com>
2022-05-04 14:25:23 +0800

    p２１

--- p10/main.go
+++ p10/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func writeExample(w http.ResponseWriter, r *http.Request) {
 	str := `<!DOCTYPE html>
@@ -16,12 +19,38 @@ func writeExample(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte(str)) // 把str 写入到body 里面， 需要类型转换
 }
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://baidu.com")
+	// 必须调用前修改header
+	w.WriteHeader(302) // 重定向302
+}
+
+type POST struct {
+	User   string
+	Thread []string
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &POST{
+		User:   "xxpkk",
+		Thread: []string{"first", "second", "third"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
 func main() {
 
 	server := http.Server{
 		Addr: "localhost:8080",
 	}
 	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/redirect", headerExample)
+
+	//json 案例
+	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
 
 }
